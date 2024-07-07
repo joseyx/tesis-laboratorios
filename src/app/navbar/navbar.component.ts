fb2f35644e83cbc7b54fdf291428c2bec5cfa7f2
José Rojas <josea.rcampero@gmail.com>
2024-07-07 03:34:46 -0400

    Importado componente about

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CarrouselComponent } from '../carrousel/carrousel.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CarrouselComponent],
+  imports: [CarrouselComponent, AboutComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
