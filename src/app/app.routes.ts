51832e9f37e70607eee49385fe9b3126fb77d9d2
José Rojas <josea.rcampero@gmail.com>
2024-07-10 11:45:15 -0400

    Agregado compoenente de error 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ChatbotComponent } from './chatbot/chatbot.component';
+import { Error404Component } from './error404/error404.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: NavbarComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'chatbot', component: ChatbotComponent}
+  {path: 'chatbot', component: ChatbotComponent},
+  {path: '**', component: Error404Component }
 ];
