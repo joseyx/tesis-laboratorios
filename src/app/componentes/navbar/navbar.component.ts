ac69465dfb5008c8fab542ebe2b1c5a6fc9c0dc7
José Rojas <josea.rcampero@gmail.com>
2024-07-22 20:15:31 -0400

    Agregada logica the autenticacion

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -8,7 +8,9 @@ import { TestimoniosComponent } from '../testimonios/testimonios.component';
 import { FooterComponent } from '../footer/footer.component';
 import { PerfilComponent } from '../perfil/perfil.component';
 import { ChatbotComponent } from '../chatbot/chatbot.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -23,11 +25,37 @@ import { RouterOutlet } from '@angular/router';
     FooterComponent,
     PerfilComponent,
     ChatbotComponent,
-    RouterOutlet
+    RouterOutlet,
+    NgIf,
+    RouterLink
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  protected isLoggedIn: boolean = false;
+  name: string | undefined;
+  isAdmin: boolean = false;
+  constructor(private authService: AuthService, ) {
+  }
 
+  async ngOnInit() {
+    this.isLoggedIn = this.authService.isUserLoggedIn();
+    this.authService.getUser().then((data) => {
+      this.name = data.name;
+      if (data.rol == 'Admin') {
+        this.isAdmin = true;
+      }
+    });
+  }
+
+  async logout() {
+    try {
+      const response = await this.authService.logout();
+      console.log(response);
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
